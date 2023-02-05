c56318a445424c847f1163e7e433386a74d4dcbc
Baimenovbn <baibn@mail.ru>
2023-02-05 22:06:27 +0600

    fix: errors fixed 3

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -15,7 +15,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Object user =  req.getSession().getAttribute("user");
+        Object user = req.getSession().getAttribute("user");
 
         if (user == null) {
             req.getRequestDispatcher("user/hello.jsp").forward(req, resp);
@@ -27,14 +27,11 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Object login = req.getAttribute("login");
-        RequestDispatcher dispatcher;
 
-        if (Users.getInstance().getUsers().contains(login) || (login != null && login.equals(""))) {
-            dispatcher = req.getRequestDispatcher("/user/hello.jsp");
+        if ((login != null && Users.getInstance().getUsers().contains(login)) || (login != null && login.equals(""))) {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
         } else {
-            dispatcher = req.getRequestDispatcher("/login.jsp");
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
         }
-
-        dispatcher.forward(req, resp);
     }
 }
--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -1,6 +1,5 @@
 package com.example.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,8 +16,8 @@ public class LogoutServlet extends HttpServlet {
         session.removeAttribute("user");
         session.invalidate();
         try {
-            req.getRequestDispatcher("/login.jsp").forward(req, resp);
-        } catch (IOException | ServletException e) {
+            resp.sendRedirect("/login");
+        } catch (IOException e) {
             e.getLocalizedMessage();
         }
     }
